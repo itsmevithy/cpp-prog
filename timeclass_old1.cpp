c6cb8fbc53fea30e24b454858db0b88fee87b1f4
itsmevithy <86781215+itsmevithy@users.noreply.github.com>
2023-04-18 12:51:32 +0530

    Add files via upload

---
+++ timeclass_old1.cpp
@@ -0,0 +1,103 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+class Time{
+	int h, m, s;
+	public:
+		Time(){
+			h=0;
+			m=0;
+			s=0;
+		}
+		Time(int a, int b, int c){
+			do{
+				if(a<24) break;
+				else{
+		        		cout<<"Enter valid hr value (0-23): ";
+					cin>>a;
+				}
+			} while(true);
+			do{
+				if(b<60) break;
+				else{
+					cout<<"Enter valid min value (0-59): ";
+					cin>>b;
+				}
+			} while(true);
+			do{
+				if(c<60) break;
+				else{
+					cout<<"Enter valid sec value (0-59): ";
+					cin>>c;
+				}
+			} while(true);
+			h=a; m=b; s=c;
+		}
+		void add(Time a, Time b){
+			h=a.h+b.h;
+			m=a.m+b.m;
+			s=a.s+b.s;
+			while((h>23)||(m>59)||(s>59)){
+				if(h>23) h-=23;
+				else if(m>59) {m-=60; h+=1;}
+				else if(s>59) {s-=60; m+=1;}
+			}
+		}
+		void display(){
+			cout<<"["<<setw(2)<<setfill('0')<<h<<':'<<setw(2)<<setfill('0')<<m<<':'<<setw(2)<<setfill('0')<<s<<"]\n";
+		}
+};
+
+int main(){
+	int a=0, b=0, c=0, choice;
+	Time r;
+
+	cout<<"Enter current time: ";
+	cin>>a>>b>>c;
+	Time p(a,b,c);
+	cout<<"Currently time is: "; p.display();
+
+	cout<<"Choose duration format:(h-hours, m-mins, s-secs, 1-hms): ";
+	cin>>choice;
+	do{
+		switch(choice){
+			case(1):
+				cout<<"Enter duration in hours: ";
+				cin>>a;
+				cout<<"After duration of "<<a<<" hours, ";
+				a%=24;
+				break;
+			case(2):
+				cout<<"Enter duration in mins: ";
+				cin>>b;
+				cout<<"After duration of "<<b<<" mins, ";
+				a+=(b/60); b%=60;
+				a%=24;
+				break;
+			case(3):
+				cout<<"Enter duration in secs: ";
+				cin>>c;
+				cout<<"After duration of "<<c<<" secs, ";
+				b+=(c/60); c%=60;
+				a+=(b/60); b%=60;			
+				a%=24;
+				break;
+			case(4):
+				cout<<"Enter duration (HMS format): ";
+				cin>>a>>b>>c;
+				Time q(a, b, c);
+				cout<<"After duration of "; q.display();
+				break;
+			default:
+				cout<<"Enter valid input!!!\n";
+				break;
+		}
+	}while((choice<1)||(choice>4));
+
+	Time q(a, b, c); 
+
+	r.add(p, q);
+	cout<<"the time will be ";
+	r.display();
+}
