d25691f836bd2977aab44a00a00f2aeba6b77da1
itsmevithy <86781215+itsmevithy@users.noreply.github.com>
2023-05-30 12:55:51 +0530

    Add files via upload

---
+++ comptext.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<math.h>
+#include<stdlib.h>
+using namespace std;
+
+class Complex{
+	private:
+		float re, im;
+	public:
+		void get(){
+			cout<<"Enter real part: ";
+			cin>>re;
+			cout<<"Enter img part: ";
+			cin>>im;
+		}
+		void show(){
+			cout<<re<<" + i"<<im;
+		}
+		float absolute_value(){
+			return sqrt((re*re)+(im*im));
+		}
+};
+
+int main(){
+	Complex *arr=(Complex*)malloc(10*sizeof(Complex));
+
+	int i, j, choice=1, n=0;
+	do{
+		arr->get();
+		cout<<"Enter 1 to continue entering values, any other key to stop..\n";
+		cin>>choice;
+		arr->show();
+		cout<<endl<<arr->absolute_value()<<endl;
+		n++; arr++;
+	} while((choice==1)&&(n<10));
+
+	for(i=0; i<n; i++) --arr;
+
+	for(i=0; i<n; i++)
+		for(j=0; j<(n-i-1); j++)
+			if((arr[j].absolute_value())<(arr[j+1].absolute_value())){
+				Complex temp=arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=temp;
+			}
+
+	cout<<"Sorting over..\n"<<"Highest is, ";
+	arr[0].show();
+	cout<<" with value "<<arr[0].absolute_value()<<endl;
+}
