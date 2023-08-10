fcee45fd92ef05a6785603affe6ffdce41bf8001
itsmevithy <86781215+itsmevithy@users.noreply.github.com>
2023-08-10 19:01:57 +0530

    Delete CmdArgDemo.java

--- data_structures_lab/CmdArgDemo.java
+++
@@ -1,9 +0,0 @@
-
-class CmdArgDemo{
-	public static void main(String arg[]){
-		int a, b;
-		a=Integer.parseInt(arg[0]);
-		b=Integer.parseInt(arg[1]);
-		System.out.println("First "+arg[0]+" Second "+arg[1]);
-	}
-}
