e4157e9bb029e9b582d32c9dc99d71cddfdd5abf
itsmevithy <86781215+itsmevithy@users.noreply.github.com>
2023-08-11 14:07:00 +0530

    Add files via upload

---
+++ data_structures_lab/insertionsort.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+void InsertionSort(int *list, int last){
+	int current=1, walker, key;
+	for(;(current<=last);current++){
+		key=list[current];
+		walker=current-1;
+		for(;((walker>=0)&&(key<list[walker]));walker--){
+			list[walker]+=list[walker+1];
+			list[walker+1]=list[walker]-list[walker+1];
+			list[walker]-=list[walker+1];		
+		}
+		list[walker+1]=key;
+	}
+}
+
+int main(){
+	int n, i;
+	cout<<"Enter number of elements in the array: ";
+	cin>>n;
+	int arr[n];
+	for(i=0; i<n; i++){
+		cout<<"Enter element "<<i+1<<" :";
+		cin>>arr[i];
+	}
+	cout<<"Before sorting: ";
+	for(i=0; i<n; i++) cout<<arr[i]<<", ";
+	InsertionSort(arr, n-1);	
+	cout<<"\nAfter sorting: ";
+	for(i=0; i<n; i++) cout<<arr[i]<<", ";
+	cout<<endl;
+}
