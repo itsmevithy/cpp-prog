3b8e7318579487f61482ca23dc2d385a88d764ae
itsmevithy <86781215+itsmevithy@users.noreply.github.com>
2023-09-15 00:39:47 +0530

    Add files via upload

--- data_structures_lab/sing_ll.cpp
+++ data_structures_lab/sing_ll.cpp
@@ -12,72 +12,122 @@ struct head{
 };
 
 class SLL{
+	head *list;
 	public:
-		bool isEmpty(head* list){
-			return (list->first==NULL)?true:false;
-		}
-		void createList(head *list){
+		void createList(){
 			list=new head();
 			list->count=0;
 			list->first=NULL;
 		}
-		bool insertNode(head *list, node *pPre, int dataIn){
-			node* pNew=new node();
-			pNew->data=dataIn;
+		bool insertNode(int dataIn){
+			node *pNew=new node;
+			if(pNew==NULL) return false;
+			else{
+				pNew->data=dataIn;
+				pNew->link=NULL;
+			}
+			node *pPre=NULL, *pLoc=NULL;
+			if(searchList(pPre, pLoc, dataIn)) return false;
 			if(pPre==NULL){
 				pNew->link=list->first;
 				list->first=pNew;
-				(list->count)++;
 			}
 			else{
 				pNew->link=pPre->link;
 				pPre->link=pNew;
-				(list->count)++;
 			}
+			(list->count)++;
 			return true;
 		}
-		void deleteNode(head* list, node *pPre, node* pLoc, int &dataOut){
-			dataOut=pLoc->data;
+		bool deleteNode(int target){
+			node *pPre, *pLoc;
+			if(!searchList(pPre, pLoc, target)) return false;
 			if(pPre==NULL) list->first=pLoc->link;
 			else pPre->link=pLoc->link;
 			delete pLoc;
 			(list->count)--;
+			return true;
 		}
-		bool searchList(head* list, node *pPre, node *pLoc, int target){
+		bool searchList(node *&pPre, node *&pLoc, int target){
 			pPre=NULL;
 			pLoc=list->first;
-			while((pLoc!=NULL)&&(target>pLoc->data)){
+			while((pLoc!=NULL)&&(target>(pLoc->data))){
 				pPre=pLoc;
 				pLoc=pLoc->link;
 			}
 			if(pLoc==NULL) return false;
-			else
-				if(target==pLoc->data) return true;
-				else return false;
+			else if(target==pLoc->data) return true;
+			else return false;
 		}
-		bool retrieveNode(head *list, int key, int&dataOut){
+		bool retrieveNode(int key, int&dataOut){
 			node *pPre, *pLoc;
-			if(searchList(list, pPre, pLoc, key)){
+			if(searchList(pPre, pLoc, key)){
 				pLoc->data=dataOut;
 				return true;
 			}
+			return false;
 		}
-		bool emptyList(head *list){
-			return (list->count==0)?true:false;
+		bool emptyList(){
+			return (list->first==NULL)?true:false;
 		}
-		int listCount(head* list){
+		int listCount(){
 			return list->count;
 		}
-		bool getNext(head *list, fromWhere, int &dataOut){
-			if(isEmpty(list)) return false;
-				
+		void getNext(){
+			node *temp=list->first;
+			while(temp!=NULL){
+				cout<<temp->data<<", ";
+				temp=temp->link;
+			}
+			cout<<endl;	
 		}
-		void destroyList(head *list){
-			while(list->count>0){
+		void destroyList(){
+			node *temp=new node;
+			while(!emptyList()){
+				temp=list->first;
 				list->first=list->first->link;
+				delete temp;
 			}
 			list->count=0;
+			cout<<"Done\n";
 		}
 };
 
-int main(){}
+int main(){
+	SLL arr;
+	arr.createList();
+	int x, choice;
+	do{
+		cout<<"Enter choice: [0-Exit, 1- Insert, 2-Delete, 3-Search, 4-Count, 5-Display, 6-Destroy]";
+		cin>>choice;
+		switch(choice){
+			case 1:
+				cout<<"Enter element to enter: ";
+				cin>>x;
+				cout<<((arr.insertNode(x))?"Done":"Failed")<<endl;
+				break;
+			case 2:
+				cout<<"Enter element to delete: ";
+				cin>>x;
+				cout<<((arr.deleteNode(x))?"Done":"Failed")<<endl;
+				break;
+			case 3:
+				cout<<"Enter element to search: ";
+				cin>>x;
+				cout<<((arr.retrieveNode(x, x))?"Found":"Not Found")<<endl;
+				break;
+			case 4:
+				cout<<"The number of elements is "<<arr.listCount()<<endl;
+				break;
+			case 5:
+				arr.getNext();
+				break;
+			case 6:
+				arr.destroyList();
+				break;				
+			default:
+				choice=0;
+				break;
+		}
+	}while(choice);
+}
