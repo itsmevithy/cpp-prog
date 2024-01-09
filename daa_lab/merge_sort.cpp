3a18cadfb5b0e2adcfff246a6fa5f770cba016c2
itsmevithy <lordvithu24@gmail.com>
2024-01-09 20:35:44 +0530

    update merge_sort.cpp

--- daa_lab/merge_sort.cpp
+++ daa_lab/merge_sort.cpp
@@ -3,7 +3,7 @@
 #include<cstdlib>
 using namespace std;
 
-void sort(int a[], int beg, int end){
+void sort(int a[], int beg, int end, int ch){
 	int mid=(beg+end)/2;
 	int i, n1=mid-beg+1;
 	int j, n2=end-mid;
@@ -14,17 +14,17 @@ void sort(int a[], int beg, int end){
 	
 	i=0; j=0; int k=beg;
 	
-	while(i<n1 && j<n2) a[k++]=(larr[i]<rarr[j])?larr[i++]:rarr[j++];
+	while(i<n1 && j<n2) a[k++]=((ch==1)?larr[i]<rarr[j]:larr[i]>rarr[j])?larr[i++]:rarr[j++];
 	while(i<n1) a[k++]=larr[i++];
 	while(j<n2) a[k++]=rarr[j++];
 }
 
-void merge(int a[], int beg, int end){
+void merge(int a[], int beg, int end, int ch){
 	if(beg<end){
 		int mid=(beg+end)/2;
-		merge(a, beg, mid);
-		merge(a, mid+1, end);
-		sort(a, beg, end);
+		merge(a, beg, mid, ch);
+		merge(a, mid+1, end, ch);
+		sort(a, beg, end, ch);
 	}
 
 }
@@ -41,13 +41,12 @@ int main(){
 	
 	ofstream fo;
 	fo.open("ascending.txt");
-	merge(arr, 0, 10000);
+	merge(arr, 0, 9999, 1);
 	for(i=0; i<10000; i++) fo<<arr[i]<<'\t';
 	fo.close();
 	
-/*	fo.open("descending.txt");
-
+	fo.open("descending.txt");
+	merge(arr, 0, 9999, -1);
 	for(i=0; i<10000; i++) fo<<arr[i]<<'\t';
 	fo.close();
-*/
 }
