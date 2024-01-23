8fa3dd1fc8fd43706ca8f61a12fc334aa44895bf
itsmevithy <lordvithu24@gmail.com>
2024-01-23 19:00:50 +0530

    update radix sort

--- daa_lab/radix-sort.cpp
+++ daa_lab/radix-sort.cpp
@@ -24,17 +24,17 @@ int * radix_sort(int a[], int n){
 }
 
 int main(){
-/*
+
 	int *arr, n;
 	cout<<"Enter numbers of elements: ";
 	cin>>n;
 	arr=new int[n];
 	cout<<"Enter the elements: ";
 	for(int i=0; i<n; i++) cin>>arr[i];
-	int* a=count_sort(arr, n);
+	int* a=radix_sort(arr, n);
 	for(int i=0; i<n; i++) cout<<a[i]<<" ";
 	cout<<endl;
-*/	
+/*	
 	ifstream fi("randno.txt");
 	int arr[10000], x;
 	for(int i=0; !fi.eof()&&i<10000; arr[i++]=x){
@@ -44,9 +44,8 @@ int main(){
 
     ofstream fo;
     fo.open("ascending.txt");
-//	int* a=count_sort(arr, 10000);
-//    for(int i=0; i<10000; i++) fo<<a[i]<<'\t';
+	int* a=radix_sort(arr, 10000);
+    for(int i=0; i<10000; i++) fo<<a[i]<<'\t';
     fo.close();
-
-
+*/
 }
