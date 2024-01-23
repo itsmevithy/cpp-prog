4496f8d49e0e4e4083da8d55bff440e9d8dfa56d
itsmevithy <86781215+itsmevithy@users.noreply.github.com>
2024-01-23 13:50:36 +0530

    Add files via upload

---
+++ quick-sort.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int partition(int a[], int p, int r){
+	int x=a[r-1];
+	int i=p-1;
+	for(int j=p; j<r-1; j++){
+		if(a[j]<=x) swap(a[++i], a[j]);
+	}
+	swap(a[++i], a[r-1]);
+	return i;
+}
+
+void quick_sort(int a[], int p, int r){
+	if(p>=r) return;
+	int q=partition(a, p, r);
+	quick_sort(a, p, q);
+	quick_sort(a, q+1, r);
+}
+
+int main(){
+/*
+	int *arr, n;
+	cout<<"Enter numbers of elements: ";
+	cin>>n;
+	arr=new int[n];
+	cout<<"Enter the elements: ";
+	for(int i=0; i<n; i++) cin>>arr[i];
+	quick_sort(arr, 0, n);
+	for(int i=0; i<n; i++) cout<<arr[i]<<" ";
+	cout<<endl;
+*/
+	
+	ifstream fi("randno.txt");
+	int arr[10000], x;
+	for(int i=0; !fi.eof()&&i<10000; arr[i++]=x){
+        fi>>x; 
+	}
+	fi.close();
+
+    ofstream fo;
+    fo.open("ascending.txt");
+    quick_sort(arr, 0, 10000);
+    for(int i=0; i<10000; i++) fo<<arr[i]<<'\t';
+    fo.close();
+}
