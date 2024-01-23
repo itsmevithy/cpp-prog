1fffc9805696822675996997c3287a736cadbe45
itsmevithy <86781215+itsmevithy@users.noreply.github.com>
2024-01-23 13:28:07 +0530

    Add files via upload

--- daa_lab/heap_manual.cpp
+++ daa_lab/heap_manual.cpp
@@ -2,75 +2,79 @@
 #include<fstream>
 using namespace std;
 
-void swap(int &x, int&y){
-	int temp=x;
-	x=y;
-	y=temp;
-}
-
-void print(int *arr, int n){
-	int p=2; 
-	//cout<<"The max heap is, \n";
-	for(int i=0; i<n; i++){
-		if(i+1==p){
-			cout<<endl;
-			p*=2;
+class Heap{
+	public:
+		int *arr, n;
+		
+		Heap(int *a, int size){
+			n=size;
+			arr=new int[n];
+			for(int i=0; i<n; i++) arr[i]=a[i];
+			for(int i=(n/2); i>=0; i--) heapify(i, n);
 		}
-		cout<<arr[i]<<"\t";
-	}
-	cout<<endl;
-}
-
-void maxheapify(int *a, int i, int size){
-	int l=2*i+1, r=2*i+1;
-	int largest;
-	largest=(l<size && a[l]>a[i])?l:i;
-	largest=(r<size && a[r]>a[i])?r:largest;
-	if(largest!=i){
-		swap(a[i], a[largest]);
-		maxheapify(a, largest, size);
-	}
-}
-
-void heapsort(int* a, int size){
-	for(int i=size/2-1; i>=0; i--)
-		maxheapify(a, i, size);
-	print(a, size);
-	cout<<"The sorted array is, ";
-	for(int i=size-1; i>0; i--){
-		swap(a[0], a[i]);
-		maxheapify(a, 0, i);
-	}
-	print(a, size);
-}
-
+		void heapify(int ind, int last){
+			int n1=2*ind+1, n2=n1+1;
+			int maxn=(n1<last && arr[n1]>arr[ind])?n1:ind;
+			maxn=(n2<last && arr[n2]>arr[maxn])?n2:maxn;
+			if(ind!=maxn){
+				swap(arr[maxn], arr[ind]);
+				heapify(maxn, last);
+			}
+		}
+		void swap(int &x, int&y){
+			int temp=x;
+			x=y;
+			y=temp;
+		}
+		
+		void sort(){
+			for(int i=n-1; i>0; i--){
+				swap(arr[i], arr[0]);
+				heapify(0, i);
+			}
+		}
+		void print(){
+			int p=2; 
+			cout<<"The max heap is, \n";
+			for(int i=0; i<n; i++){
+				if(i+1==p){
+					cout<<endl;
+					p*=2;
+				}
+				cout<<arr[i]<<"\t";
+			}
+			cout<<endl;
+			sort();
+			cout<<"The sorted array is, ";
+			for(int i=0; i<n; i++) cout<<arr[i]<<" ";
+			cout<<endl;
+		}
+};
 
 int main(){
-
+/*
 	int *arr, n;
 	cout<<"Enter numbers of elements: ";
 	cin>>n;
 	arr=new int[n];
 	cout<<"Enter the elements: ";
 	for(int i=0; i<n; i++) cin>>arr[i];
-
-/*
+	Heap h(arr, n);
+	h.print();
+*/
 	ifstream fi("randno.txt");
-    int arr[10000], x;
-    for(int i=0; !fi.eof()&&i<10000; arr[i++]=x){
+	int arr[10000], x;
+	for(int i=0; !fi.eof()&&i<10000; arr[i++]=x){
         fi>>x; 
-    }
-    fi.close();
+	}
+	fi.close();
 
-	heapsort(arr, 10000);
+	Heap h(arr, 10000);
+//	h.print();
 
     ofstream fo;
     fo.open("ascending.txt");
-	for(int i=0; i<10000; i++) fo<<arr[i]<<'\t';
+    h.sort();
+    for(int i=0; i<10000; i++) fo<<h.arr[i]<<'\t';
     fo.close();
-*/
-	heapsort(arr, n);
-//	print(arr, n);
 }
-
-
