ee4e3b03a10137b2faf73708d01f4539e1d62427
Vithyaathar <86781215+itsmevithy@users.noreply.github.com>
2024-04-09 15:09:23 +0530

    Add files via upload

---
+++ daa_lab/kcpsll-modified.cpp
@@ -0,0 +1,183 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+struct itm{
+	float w, v, vbw;
+};
+
+struct ND{ 
+	int lvl;
+	float wu, vg, ub;
+	ND *left, *right, *parent; 
+};
+
+struct node{
+	ND* data;
+	node *link;
+};
+
+struct head{
+	int count;
+	node *first;
+};
+
+class SLL{
+	public:
+		head *list;
+		SLL(){
+			list=new head();
+			list->count=0;
+			list->first=NULL;
+		}
+		bool insertNode(ND* dataIn){
+			node *pNew=new node;
+			if(pNew==NULL) return false;
+			else{
+				pNew->data=dataIn;
+				pNew->link=NULL;
+			}
+			node *pPre=NULL, *pLoc=NULL;
+			if(searchList(pPre, pLoc, dataIn)) return false;
+			if(pPre==NULL){
+				pNew->link=list->first;
+				list->first=pNew;
+			}
+			else{
+				pNew->link=pPre->link;
+				pPre->link=pNew;
+			}
+			(list->count)++;
+			return true;
+		}
+		bool deleteNode(ND* target){
+			node *pPre, *pLoc;
+			if(!searchList(pPre, pLoc, target)) return false;
+			if(pPre==NULL) list->first=pLoc->link;
+			else pPre->link=pLoc->link;
+			delete pLoc;
+			(list->count)--;
+			return true;
+		}
+		bool searchList(node *&pPre, node *&pLoc, ND* target){
+			pPre=NULL;
+			pLoc=list->first;
+			while((pLoc!=NULL)&&(target->ub < (pLoc->data->ub))){
+				pPre=pLoc;
+				pLoc=pLoc->link;
+			}
+			if(pLoc==NULL) return false;
+			else if(target==pLoc->data) return true;
+			else return false;
+		}
+		bool emptyList(){
+			return (list->first==NULL)?true:false;
+		}
+		int listCount(){
+			return list->count;
+		}
+};
+
+bool ndcomp(ND* a, ND* b){
+	return (b->ub > a->ub);
+}
+
+ND* bf_bb_knapsack(itm* item, float wl, int size){
+	ND *root=new ND, *nd=new ND;
+	root->wu=root->vg=0;
+	root->ub=wl*item[0].vbw;
+	root->lvl=0;
+	root->left=root->parent=root->right=NULL;
+	SLL q;
+	q.insertNode(root);
+	int clvl=0;
+	while(clvl<=size && !q.emptyList()){
+		nd=q.list->first->data;
+		q.deleteNode(q.list->first->data);
+		//cout<<"\n Values of extracted nd are, lvl="<<nd->lvl<<" wu="<<nd->wu<<" vg="<<nd->vg<<" ub="<<nd->ub<<endl;
+		clvl=nd->lvl+1;
+		if(clvl>size) break;
+		ND *lchild=new ND, *rchild=new ND;
+		//cout<<"So far so good!\n";
+		lchild->wu=nd->wu+item[clvl-1].w;
+		//cout<<nd->wu+item[clvl].w<<endl;
+		//cout<<"So far so good!\n";
+		if(lchild->wu<=wl){
+			lchild->vg=nd->vg+item[clvl-1].v;
+			lchild->parent=nd;
+			lchild->lvl=nd->lvl+1;
+			if(clvl==size)
+				lchild->ub=lchild->vg;
+			else
+				lchild->ub=lchild->vg+(wl-lchild->wu)*item[clvl-1].vbw;
+			lchild->left=lchild->right=NULL;
+			nd->left=lchild;
+			//q.push_back(lchild);
+			q.insertNode(lchild);
+		}	
+		rchild->wu=nd->wu;
+		rchild->vg=nd->vg;
+		rchild->parent=nd;
+		rchild->lvl=nd->lvl+1;
+		if(clvl==size)
+			rchild->ub=rchild->vg;
+		else
+			rchild->ub=rchild->vg+(wl-rchild->wu)*item[clvl-1].vbw;
+		rchild->left=rchild->right=NULL;
+		nd->right=rchild;
+		//q.push_back(rchild);
+		q.insertNode(rchild);
+	}
+	return nd;
+}
+
+void rec_print(ND* nn){
+	if(nn->parent != NULL) 
+	 {
+	  rec_print(nn->parent);
+	  if(nn ==nn->parent->left)
+		cout<<nn->lvl<<", ";
+     }
+}
+
+void bb_knapsack_sol(itm* &item, float wl, int size){
+	for(int i=0; i<size; i++)
+		item[i].vbw=item[i].v/item[i].w;
+	cout<<"\nBefore sorting,\nV: ";
+	for(int i=0; i<size; i++) cout<<item[i].v<<"\t";
+	cout<<"\nW: ";
+	for(int i=0; i<size; i++) cout<<item[i].w<<"\t";
+	cout<<"\nB: ";
+	for(int i=0; i<size; i++) cout<<item[i].vbw<<"\t";
+	itm key;
+	for(int i=1, j; i<size; i++, item[j+1]=key)
+		for(j=i-1, key=item[i]; j>=0 && key.vbw>item[j].vbw; j--) item[j+1]=item[j];
+	cout<<"\nAfter sorting\nV: ";
+	for(int i=0; i<size; i++) cout<<item[i].v<<"\t";
+	cout<<"\nW: ";
+	for(int i=0; i<size; i++) cout<<item[i].w<<"\t";
+	cout<<"\nB: ";
+	for(int i=0; i<size; i++) cout<<item[i].vbw<<"\t";
+	ND* nn=bf_bb_knapsack(item, wl, size);
+	cout<<"\nTotal profit is: "<<nn->ub<<"\nItems in knapsack are, ";
+	rec_print(nn);
+}
+
+int main(){
+	int n;
+	cout<<"Enter number of items: ";
+	cin>>n;
+	itm* item=new itm[n];
+	for(int i=0; i<n; i++){
+		cout<<"Enter weight of item "<<i+1<<" :";
+		cin>>item[i].w;
+		cout<<"Enter value of item "<<i+1<<" :";
+		cin>>item[i].v;
+	}
+	float wl;
+	cout<<"Enter WL: ";
+	cin>>wl;
+	bb_knapsack_sol(item, wl, n);
+	cout<<endl;
+}
